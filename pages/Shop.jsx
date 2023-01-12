62a4a98ea6cde409db22ce713f753df673a1d220
jaemdessources <pianodessources@gmail.com>
2023-01-12 15:02:02 -0500

    Initialisation de la page shop. Création de dossier utils pour les constante et fonction utiles. j'ai installé axios et framer-motion pour les requettes api  et animation respectivement

--- pages/Shop.jsx
+++ pages/Shop.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import Navbar from "../components/Navbar"
+import React from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import ProductFilters from "../components/ProductFilters";
+import { coffeesQueryURL } from "../utils/queries";
 
 const Shop = () => {
+  const [coffees, setCoffees] = React.useState([]);
+  React.useEffect(() => {
+    axios.get(coffeesQueryURL).then((data) => setCoffees(data.data.data));
+  }, []);
+  console.info(coffees);
   return (
     <>
-    <Navbar/>
-    <div>
-      Shop
-    </div>
+      <Navbar />
+      <main>
+        <ProductFilters />
+        <div className="products">
+          {coffees?.map(({ id, attributes }) => (
+            <div key={id}>
+              <img src={attributes.main_image?.data?.attributes?.formats?.small.url} alt="" />
+              <div>{attributes.name}</div>
+              <div>${attributes.price}</div>
+              {attributes.reviews ? <span>{attributes.reviews} reviews</span> : ""}
+            </div>
+          ))}
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
