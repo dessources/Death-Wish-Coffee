050ad6db2e109a2233220f8d8031aea10368126b
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-01-17 15:33:04 +0100

    ajoute de la gallery

--- components/CoffeeCulture.jsx
+++ components/CoffeeCulture.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import gallery01 from "../images/gallery01.webp";
+import gallery02 from "../images/gallery02.webp";
+import gallery03 from "../images/gallery03.webp";
+import gallery04 from "../images/gallery04.jpg";
+import Image from "next/image";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+
+
+const galleryImages = [gallery01, gallery02, gallery03, gallery04];
 
 const CoffeeCulture = () => {
-  return (
-    <div>Coffee Culture</div>
-  )
-}
+  const scrollRef = React.useRef(null);
 
-export default CoffeeCulture
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollLeft -= 1415;
+    } else {
+      current.scrollLeft += 1415;
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="title-gallery">Coffee Culture</h1>
+      <div className="CoffeeCulture">
+        <div className="gallery">
+          <div className="gallery-container" ref={scrollRef}>
+            {galleryImages.map((image, index) => (
+              <div
+                className="gallery-images-card"
+                key={`gallery_image-${index + 1}`}
+              >
+                <Image src={image} alt="" /> 
+              </div>
+            ))}
+          </div>
+          <div className="gallery-icons">
+            <ArrowCircleLeftIcon
+              className="arrow-icon"
+              onClick={() => scroll("left")}
+            />
+            <ArrowCircleRightIcon
+              className="arrow-icon"
+              onClick={() => scroll("right")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCulture;
