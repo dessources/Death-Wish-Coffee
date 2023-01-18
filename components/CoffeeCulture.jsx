e8fc24313e8fda0883cb7431ad7fb1d9e95536cc
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-01-18 20:19:58 +0100

    commit

--- components/CoffeeCulture.jsx
+++ components/CoffeeCulture.jsx
@@ -4,53 +4,67 @@ import gallery02 from "../images/gallery02.webp";
 import gallery03 from "../images/gallery03.webp";
 import gallery04 from "../images/gallery04.jpg";
 import Image from "next/image";
-import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
-import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 const galleryImages = [gallery01, gallery02, gallery03, gallery04];
 
 const CoffeeCulture = () => {
-  const scrollRef = React.useRef(null);
 
-  const scroll = (direction) => {
-    const { current } = scrollRef;
-
-    if (direction === "left") {
-      current.scrollLeft -= 1415;
-    } else {
-      current.scrollLeft += 1415;
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    centerMode: true,
+    centerPadding: '10%',
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1224,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
+
   return (
-    <div>
-      <h1 className="title-gallery">Coffee Culture</h1>
-      <div className="CoffeeCulture">
-        <div className="gallery">
-          <div className="gallery-container" ref={scrollRef}>
+    <div className="CoffeeCulture">
+      <h1 className="title-Coffee-Culture">COFFEE CULTURE</h1>
+      <Slider {...settings}>
+      
             {galleryImages.map((image, index) => (
-              <div
-                className="gallery-images-card"
-                key={`gallery_image-${index + 1}`}
-              >
-                <Image src={image} alt="" /> 
-              </div>
-            ))}
+              <div className="image">
+                <Image src={image} alt="" />
+              </div>          
+            ))}          
+          </Slider>
           </div>
-          <div className="gallery-icons">
-            <ArrowCircleLeftIcon
-              className="arrow-icon"
-              onClick={() => scroll("left")}
-            />
-            <ArrowCircleRightIcon
-              className="arrow-icon"
-              onClick={() => scroll("right")}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    
   );
 };
 
